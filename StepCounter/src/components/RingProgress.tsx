9869da3fc3393cc84d07126ac12a3bcef3c70b34
Noah Amoo <yawnoah3n@yahoo.com>
2025-10-27 19:17:14 +0000

    Drawn the circle with react native svg

--- StepCounter/src/components/RingProgress.tsx
+++ StepCounter/src/components/RingProgress.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Svg, { Circle } from 'react-native-svg'
 
 type RingProgressProp = {
     radius?: number;
@@ -9,8 +10,14 @@ const color = "#EE0F55"
 
 const RingProgress = ({radius = 100}: RingProgressProp) => {
   return (
-    <View style={{ width: radius * 2, height: radius * 2, backgroundColor: color, alignSelf: 'center' }}>
-      <Text style={{color: 'white'}}>RingProgress</Text>
+    <View style={{ 
+        width: radius * 2, 
+        height: radius * 2,  
+        alignSelf: 'center',
+    }}>
+        <Svg>
+            <Circle cx={radius} cy={radius} r={radius} fill={color} />
+        </Svg>
     </View>
   )
 }
